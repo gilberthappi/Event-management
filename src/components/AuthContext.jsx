ecd302c404fa17259c27e8aaec514f03a34fed06
gilberthappi <gdushimimana6@gmail.com>
2024-04-27 23:38:46 +0200

    Update auth check

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -1,20 +1,36 @@
-import React, { createContext, useState, useContext } from "react";
+// AuthContext.jsx
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userToken, setUserToken] = useState("");
 
-  const login = () => {
+  // Function to login
+  const login = (token) => {
+    setUserToken(token);
     setIsLoggedIn(true);
+    localStorage.setItem("userToken", token); // Store the token in local storage
   };
 
+  // Function to logout
   const logout = () => {
+    setUserToken("");
     setIsLoggedIn(false);
+    localStorage.removeItem("userToken"); // Remove token from local storage
   };
+
+  // Check if user is logged in when component mounts
+  useEffect(() => {
+    const token = localStorage.getItem("userToken"); // Retrieve token from local storage
+    if (token) {
+      login(token);
+    }
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, userToken }}>
       {children}
     </AuthContext.Provider>
   );
